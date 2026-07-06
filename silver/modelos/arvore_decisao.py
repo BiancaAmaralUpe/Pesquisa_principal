8a3ab081d4b41008d12d73c3dba43ba0db049fab
Bianca <bianca.amaral@upe.br>
2026-07-05 21:03:06 -0300

    organização

--- silver/modelos/arvore_decisao.py
+++ silver/modelos/arvore_decisao.py
@@ -7,7 +7,6 @@
 # - Calcular métricas iniciais de avaliação
 # ======================================================================================
 
-
 import numpy as np
 import pandas as pd
 from datetime import datetime
